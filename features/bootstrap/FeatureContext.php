57fa13bed1192662167874d79098bc81efe1a4b1
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-02-04 21:07:18 +0100

    Added composer.json

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -3,20 +3,49 @@
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use App\Square;
+use PHPUnit\Framework\TestCase;
 
 /**
- * Defines application features from the specific context.
+ * Class SquareContext
  */
-class FeatureContext implements Context
+class FutureContext implements Context
 {
     /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
+     * @var Square
      */
-    public function __construct()
+    private $square;
+
+    /**
+     * @param float $a
+     * @Given Length is :a
+     */
+    public function createNewObject(float $a): void
     {
+        $this->square = new Square($a);
+    }
+
+    /**
+     * @param float $a
+     * @throws Exception
+     * @Then Excepted calculate area is :a
+     */
+    public function calculateArea(float $a): void
+    {
+        TestCase::assertEquals($a, $this->square->calc());
+    }
+
+    /**
+     * @Then Excepted Exception
+     */
+    public function argumentsTooSmall(): void
+    {
+        try {
+            $this->square->calc();
+
+            TestCase::assertTrue(false);
+        } catch (Exception $exception) {
+            TestCase::assertTrue(true);
+        }
     }
 }
