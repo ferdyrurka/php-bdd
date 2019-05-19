5aafc8ea4c9414eabeb0453ae651ad75c3f5e950
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-05-19 22:21:48 +0200

    ShopException

---
+++ src/Shop/Exception/ShopException.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Shop\Exception;
+
+use \Exception;
+
+/**
+ * Class ShopException
+ * @package App\Shop\Exception
+ */
+class ShopException extends Exception
+{
+}
