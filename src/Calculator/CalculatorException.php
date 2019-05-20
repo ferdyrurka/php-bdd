5fc8ecb88e8808577bd59479471160a5e8fe8a14
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-05-20 20:09:31 +0200

    CalculatorException

---
+++ src/Calculator/CalculatorException.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Calculator;
+
+use \Exception;
+
+/**
+ * Class CalculatorException
+ * @package App\Calculator
+ */
+class CalculatorException extends Exception
+{
+}
