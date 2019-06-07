6fe9f19f6cd3b59afff03391e6112af69c843756
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-06-07 21:20:53 +0200

    TaxServiceSpec

---
+++ spec/Shop/Service/TaxServiceSpec.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace spec\App\Shop\Service;
+
+use App\Shop\Exception\PriceLessOrEqualNilException;
+use App\Shop\Service\TaxCountry\TaxCountryInterface;
+use PhpSpec\ObjectBehavior;
+use \Mockery;
+
+/**
+ * Class TaxServiceSpec
+ * @package spec\App\Shop\Service
+ */
+class TaxServiceSpec extends ObjectBehavior
+{
+    /**
+     * @param TaxCountryInterface $taxCountry
+     * @throws \App\Shop\Exception\PriceLessOrEqualNilException
+     */
+    public function it_is_tax_calculate_ok(TaxCountryInterface $taxCountry): void
+    {
+        $taxCountry->getTax()->willReturn(23);
+        $this->beConstructedWith($taxCountry);
+
+        $this->calculate(10.00)->shouldReturn(12.30);
+    }
+
+    /**
+     * @dataProvider get_small_prices
+     * @param float $price
+     */
+    public function it_is_price_to_small($price): void
+    {
+        $taxCountry = Mockery::mock(TaxCountryInterface::class);
+        $taxCountry->shouldReceive('getTax')->once()->andReturn(23);
+
+        $this->beConstructedWith($taxCountry);
+
+        $this->shouldThrow(PriceLessOrEqualNilException::class)
+            ->during('calculate', [$price])
+        ;
+    }
+
+    /**
+     * @return array
+     */
+    public function get_small_prices(): array
+    {
+        return [
+            -1.00,
+            0.00
+        ];
+    }
+}
